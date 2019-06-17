84831caa0ed3b633bf73edb23126f6ea8a94a5ae
yoavos777 <yoav.morgenstern@gmail.com>
2019-06-17 22:22:46 +0300

    Update client_gui.py

--- client_gui.py
+++ client_gui.py
@@ -5,35 +5,24 @@ import time
 
         
 class Application(object):
-    def command_color(self,color):
-        self.hexcolor = "FFFFFF"
-        if color == "black":
-            self.hexcolor = "000000"
-        elif color == "red":
-            self.hexcolor = "FF0000"
-        elif color == "blue":
-            self.hexcolor = "0000FF"
-        elif color == "green":
-            self.hexcolor = "00FF00"
-        elif color == "yellow":
-            self.hexcolor = "FFFF00"
-        elif color == "orange":
-            self.hexcolor = "D35400"
-        elif color == "grey":
-            self.hexcolor = "ABB2B9"
-        elif color == "pink":
-            self.hexcolor = "FFC0CB"
-        elif color == "purple":
-            self.hexcolor = "8E44AD"
-        elif color == "white":
-            self.hexcolor = "FFFFFF"
-        
+       
     def createWidgets(self):
         self.main_frame.pack(fill=BOTH, expand=True)
         canvas = Canvas(self.main_frame, bg="white")
         chat_canvas = Canvas(canvas, bg = "white", height=600, width=210,scrollregion=(0,0,10000,10000))
         chat_canvas.place(x=592,y=0)
+        self.board_canvas = Canvas(canvas, bg = "white", height=600, width=116,scrollregion=(0,0,10000,10000))
+        self.board_canvas.place(x=0,y=0)
+        
+        self.chat_text = Text(self.chat_canvas, height=35, width=25,bg="white",fg="black")
+        self.chat_text.place(x=1,y=0)
+        self.chat_text.config(state=DISABLED)
 
+        self.board_text = Text(self.board_canvas, height=40, width=14,bg="white",fg="black")
+        self.board_text.place(x=1,y=0)
+        self.board_text.insert(END,"LEADER BOARD:\n\n")
+        self.board_text.config(state=DISABLED)
+        
         j=0
         for i, tool in enumerate(self.tool_box):
             tool_button = Button(canvas, width=31, height=2, bg="black",fg="white",text=tool,command = partial(self.command_color, "white"))
@@ -48,7 +37,7 @@ class Application(object):
             
         self.chat_entry = Entry(chat_canvas, bd =5)
         self.chat_entry.place(x=1,y=570)
-        self.master.bind('<Return>',self.writing)
+        self.master.bind('<Return>',self.send_message)
         
         
         line1 = canvas.create_rectangle(120,0,130,600,fill="black") 
@@ -73,6 +62,20 @@ class Application(object):
             self.chat_text.insert(END,new_msg+"\n")
             self.chat_text.config(state=DISABLED)
         
+    def writing_board(self):
+        msg = self.cls.get_command().strip("\n\r")
+        is_get_p = ""
+        for i in range(10):
+            is_get_p += msg[i]
+            
+        if(is_get_p == "getplayers"):    
+            self.board_text.config(state=NORMAL)
+            split_msg = msg.split()
+            for i in range(1,len(split_msg)):
+                split_newmsg = split_msg[i].split(',',1)
+                self.board_text.insert(END,split_newmsg[0] + ": " +  split_newmsg[1] + "\n")
+            self.board_text.config(state=DISABLED)
+        
     def destroy_master(self):
         self.master.destroy()
            
@@ -93,6 +96,7 @@ class Application(object):
         self.command_color(self.mouse_color)
         self.createWidgets()
         self.writing_chat()
+        self.writing_board()
         #self.master.bind('<B1-Motion>',self.motion)
         self.master.mainloop()
 
